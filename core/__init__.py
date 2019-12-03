307ef770ae4ce571f74f3f68d76baa2f8d7845c7
ada92 <adadesions@gmail.com>
2019-12-04 05:57:07 +0700

    Generate Gabor Parameter by DataFrame

--- core/__init__.py
+++ core/__init__.py
@@ -4,16 +4,33 @@ import pandas as pd
 pi = np.pi
 
 scales = (512, 256, 128, 64)
-thetas = (0, pi/6, pi/4, pi/6, pi/2, 2*pi/3, 3*pi/4, 5*pi/6, pi)
+thetas = [0, pi/6, pi/4, pi/6, pi/2, 2*pi/3, 3*pi/4, 5*pi/6, pi]
 sigmas = (4, 16, 32, 64)
 lambdas = ((1, 2), (2, 4), (4, 8), (16, 32))
 
+lenSigma, lenTheta, lenLambda = len(sigmas), len(thetas), 2
+maxLen = lenSigma*lenTheta*lenLambda
+
+
 params = {
-    'Scale': np.merge(np.repeat(512, 4), np.repeat(256, 3)),
-    'Sigma': [4, 16, 32, 64, 4, 16, 32]
+    'Scale': np.concatenate((
+        np.repeat(512, 4*2*lenTheta), # lenSigma x lenLambda x lenTheta
+        np.repeat(256, 4*2*lenTheta),
+    )),
+    'Sigma': np.concatenate((
+        np.repeat([4, 16, 32, 64], 2*lenTheta, axis=0).flatten(),
+        np.repeat([4, 16, 32, 64], 2*lenTheta, axis=0).flatten(),
+    )),
+    'Lambda': np.concatenate((
+        np.repeat([[1, 2]], 4*lenTheta, axis=0).flatten(), # lenSigma x lenTheta
+        np.repeat([[2, 4]], 4*lenTheta, axis=0).flatten(),
+    )),
+    'Theta': np.repeat([thetas], 4*2*2, axis=0).flatten(), # lenSigma x lenLambda 
 }
 
-print(params)
-df = pd.DataFrame(params)
+gaborParams = pd.DataFrame(params).sort_values(['Scale', 'Lambda', 'Sigma', 'Theta'])
 
-print(df)
+print(gaborParams)
+
+with open('gaborParams.csv', 'w') as file:
+    file.write(gaborParams.to_csv(index=False))
--- main.py
+++ main.py
@@ -12,16 +12,16 @@ img_path = 'data/kaggle_3m/TCGA_CS_4941_19960909/TCGA_CS_4941_19960909_11.tif'
 
 if __name__ == '__main__':
     img_block = im.getImage(img_path, display=False)
-    # hc_index = hc_ext.get_hc_multi_index()
-    # reduces_block = im.multiPyrDown(img_block[1])[::-1]
-    # transformed_block = hc_ext.transform(reduces_block, hc_index)
+    hc_index = hc_ext.get_hc_multi_index()
+    reduces_block = im.multiPyrDown(img_block[1])[::-1]
+    transformed_block = hc_ext.transform(reduces_block, hc_index)
 
-    # cgraph.grid_plot(transformed_block, 3, title='HC-Analysis')
-    # cgraph.grid_plot(reduces_block, 3, is_img=True, title='Images')
+    cgraph.grid_plot(transformed_block, 3, title='HC-Analysis')
+    cgraph.grid_plot(reduces_block, 3, is_img=True, title='Images')
 
-    gabor_set = cgabor.getKernel(20, 20)
-    cgraph.grid_plot2(gabor_set, 9, 'Gabor Jets')
-    gabor_plot = []
+    # gabor_set = cgabor.getKernel(20, 20)
+    # cgraph.grid_plot2(gabor_set, 9, 'Gabor Jets')
+    # gabor_plot = []
 
     # for i in np.arange(0, len(gabor_set)):
     #     garbor_img = cv2.filter2D(img_block[2], -1, gabor_set[i])
